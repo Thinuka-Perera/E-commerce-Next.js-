c627ff105c5c09004ab6a3ec53417261bd70a885
Thinuka-Perera <thinukapereraa@gmail.com>
2025-11-21 21:58:54 +0530

    Done Hero Section and Product Cards

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,25 @@
-const Homepage = () => {
-  return (
-    <div className=''>Homepage</div>
-  )
-}
+import ProductList from "@/components/ProductList";
+import Image from "next/image";
 
-export default Homepage
+const Homepage = async ({searchParams}:{searchParams: Promise<{category:string}>; }) => {
+
+  const category = (await searchParams).category
+
+
+  return (
+    <div className="">
+      <div className="relative aspect-[3/1] mt-2 mb-12 overflow-hidden rounded-lg shadow-lg">
+        <Image
+          src="/featured.png"
+          alt="Featured Product"
+          fill
+          className=" object-center transition-transform duration-500 hover:scale-105"
+          priority
+        />
+      </div>
+      <ProductList category={category}/>
+    </div>
+  );
+};
+
+export default Homepage;
